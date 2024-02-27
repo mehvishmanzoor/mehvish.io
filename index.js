29a34f381abfcb792fb4fc3dc95c3af2b2da7609
mehvishmanzoor <161422284+mehvishmanzoor@users.noreply.github.com>
2024-02-27 09:56:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const imageOverlay = document.getElementById('image-overlay');
 // Replace these URLs with the actual URLs of your art pieces
 const artPieces = [
     'https://mehvishmanzoor.github.io/mehvish.io/hatsApp Image 2024-02-27 at 09.14.32_d57f4327.jpg',
-    'https://mehvishmanzoor.github.io/mehvish.io/.jpg',
+    'https://mehvishmanzoor.github.io/mehvish.io/WhatsApp Image 2024-02-27 at 09.14.33_06e555ae.jpg',
     'https://mehvishmanzoor.github.io/mehvish.io/.jpg',
     // Add more art piece URLs as needed
 ];
